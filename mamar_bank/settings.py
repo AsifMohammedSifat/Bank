8bde7ece7a4de0c6fae22342f24c8ad8916000ff
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2023-12-31 20:29:33 +0600

    database added

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,17 +99,27 @@ env = environ.Env()
 environ.Env.read_env()
 ...
 # Your secret key
-SECRET_KEY = env("SECRET_KEY")
-...
+# SECRET_KEY = env("SECRET_KEY")
+# ...
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgres://bd_bank_db_user:hr628ymrbvFlbbwykL3FhILAAazA6Sio@dpg-cm8nkq0cmk4c7392tg4g-a.oregon-postgres.render.com/bd_bank_db',
+    )
 }
 
 # Password validation
